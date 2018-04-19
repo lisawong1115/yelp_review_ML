fbe9fa9d73590f2b8056dccbc9d24989360fcd98
Lisa Huang <lisawong1115@gmail.com>
2018-04-19 09:52:18 -0400

    keep only restaurant reviews with length between 100-200 characters, split them into equal number of useful and useless reviews.

--- dataProcessor.py
+++ dataProcessor.py
@@ -7,13 +7,14 @@
 
 import json
 import re
+import operator
 
 nonspace = re.compile(r'\S')
 def iterparse(j):
     decoder = json.JSONDecoder()
     pos = 0
     counter = 0 
-    while True and counter<20000:
+    while True and counter<2000:
         matched = nonspace.search(j, pos)
         if not matched:
             break
@@ -26,41 +27,167 @@ def writeToJSON(filename, newFile):
     with open(filename, "r") as data_file:
         rawdata = data_file.read()
 
-    data = {"data": []}
+    data = []
     for decoded in iterparse(rawdata):
-        data["data"].append(decoded)
+        data.append(decoded)
     # print("data length: (expected 100)" + str(len(data)))
     # j = json.dumps(data)
     with open(newFile, "w") as file:
         json.dump(data, file)
 
-def divideData(filename, voteFile, nvFile):
-    with open(filename, "r") as json_data:
-        d = json.load(json_data)
-        json_data.close()
-        # d["data"] is a list of dictionaries
-    vCount = 0
-    nCount = 0
-    withVotes = []
-    noVotes = []
-    for dct in d["data"]:
-        if dct[u"useful"] == 0 and dct[u"funny"] == 0 and dct[u"cool"] == 0 and nCount < 5000:
-            noVotes.append(dct)
-            nCount += 1
+def sortBusiness(filename):
+    with open(filename, "r") as file:
+        data = json.load(file)
+    result = {}
+    for d in data:
+        if d["business_id"] not in result:
+            bID = d["business_id"]
+            result[bID] = d["categories"]
+    return result
+
+def findCategories(businessDict):
+    result = []
+    for key in businessDict: #
+        categories = businessDict[key]
+        for category in categories:
+            result.append(category)
+    return list(set(result))
+
+def frequencyOfCategories(reviewFile, businessFile):
+    businessDict = sortBusiness(businessFile)
+    categories = findCategories(businessDict)
+
+    with open(reviewFile, "r") as f:
+        reviews = json.load(f)
+
+    frequencyDict = {}
+
+    for category in categories:
+        if category not in frequencyDict:
+            frequencyDict[category] = 0
+
+    for review in reviews:
+        businessID = review["business_id"]
+        if businessID in businessDict:
+            categoriesOfOne = businessDict[businessID]
+
+            for c in categoriesOfOne:
+                frequencyDict[c] += 1
+
+    return frequencyDict
+
+def findBusinessByCategory(businessFile, category):
+    businessDict = sortBusiness(businessFile)
+    idList = []
+    for key in businessDict:
+        if category in businessDict[key]:
+            idList.append(key)
+    print("length of idList: " + str(len(idList)))
+    return idList
+
+def findReviewsByID(reviewFile, idList, outFile):
+    with open(reviewFile, "r") as f:
+        reviews = json.load(f)
+    targets = []
+    for review in reviews:
+        if review["business_id"] in idList:
+            targets.append(review)
+    print("number of restaurant reviews: " + str(len(targets)))
+
+    with open(outFile, "w") as file:
+        json.dump(targets, file)
+
+def getReviewLength(reviewFile):
+    with open(reviewFile, "r") as f:
+        reviews = json.load(f)
+    distribution = {}
+    for review in reviews:
+        text = review["text"]
+        length = len(text)
+        if "character count" not in review:
+            review["character count"] = length
+        lowerRange = length/100*100
+        lenRange = str(lowerRange) + "-" + str(lowerRange+100)
+        if lenRange not in distribution:
+            distribution[lenRange] = 1
         else:
-            if vCount < 5000:
-                withVotes.append(dct)
-                vCount += 1
-    print("length of votes:" + str(len(withVotes)))
-    print("length of noVotes:" + str(len(noVotes)))
-    with open(voteFile, "w") as vf:
-        json.dump(withVotes, vf)
-    with open(nvFile, "w") as nf:
-        json.dump(noVotes, nf)
-    # print("Yeah")
+            distribution[lenRange] += 1
+    maximum = max(distribution.iteritems(), key=operator.itemgetter(1))
+    return maximum
+
+def reviewByLength(inFile, outFile):
+    with open(inFile, "r") as f:
+        data = json.load(f)
+    reviews = []
+    for review in data:
+        text = review["text"]
+        length = len(text)
+        if "character count" not in review:
+            review["character count"] = length
+        if length >= 100 and length <=  200:
+            reviews.append(review)
+    with open(outFile, "w") as f:
+        json.dump(reviews, f)
+    return len(reviews)
+
+def countVote(inFile):
+    with open(inFile, "r") as f:
+        data = json.load(f)
+    # useful = []
+    # useless = []
+    result = {"useful": 0, "useless": 0}
+    for review in data:
+        if review["useful"] > 0:
+            result["useful"] += 1
+        else:
+            result ["useless"] += 1
+    return result
+
+def splitFile(inFile, out1, out2):
+    with open(inFile, "r") as f:
+        data = json.load(f)
+    useful = []
+    useless = []
+    for review in data:
+        if review["useful"] > 0 and len(useful) < 130000:
+            useful.append(review)
+        elif review["useful"] == 0 and len(useless) < 130000:
+            useless.append(review)
+
+    with open(out1, "w") as f1:
+        json.dump(useful, f1)
+    with open(out2, "w") as f2:
+        json.dump(useless, f2)
+    return (len(useful), len(useless))
 
 
 if __name__ == "__main__":
-    filename = "review.json"
+    # print(splitFile("restaurant_reviews_100-200.json", "useful.json", "useless.json"))
+
+    # print(countVote("restaurant_reviews_100-200.json")) #{'useful': 139907, 'useless': 397023}
+    # print(reviewByLength("restaurant_reviews.json", "restaurant_reviews_100-200.json"))
+
+    # maximum = getReviewLength("restaurant_reviews.json")
+    # print(maximum) #('100-200', 531573)
+
+    # reviewFile = "reviewJSON.json"
+    # idList = findBusinessByCategory("businessJSON.json", "Restaurants")
+    # outFile = "restaurant_reviews.json"
+    # findReviewsByID(reviewFile, idList, outFile)
+
+    # d = frequencyOfCategories("reviewJSON.json", "businessJSON.json")
+    # maximum = max(d.iteritems(), key=operator.itemgetter(1))
+    # print(maximum) # (u'Restaurants', 3221419)
+
+    # with open ("sample.json", "r") as f:
+    #     d = json.load(f)
+    #     newData = d["data"]
+    # with open("new_review_sample.json", "w") as file:
+    #     json.dump(newData, file)
+    # writeToJSON("review.json", "reviewJSON.json")
+    # print(len(categories))
+    # writeToJSON(filename, "businessJSON.json")
+    # with open ("businessCategories.json", "w") as file:
+    #     json.dump(categories, file)
     # writeToJSON(filename, "newReviewTesting3.json")
     # divideData("newReviewTesting3.json", "vf2.json", "nf2.json")
